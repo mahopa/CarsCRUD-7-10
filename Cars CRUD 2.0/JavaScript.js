1da58a1fdaf171218aff4a7ecced3718fe675aa0
Mahopa <mahopa@gmail.com>
2014-07-11 16:09:13 -0500

    Added Update in Modal

--- Cars CRUD 2.0/JavaScript.js
+++ Cars CRUD 2.0/JavaScript.js
@@ -14,7 +14,8 @@ CarApp.drawTable = function () {
         holder += "<tr>";
         holder += "<td>" + CarApp.cars[c].model + "</td>";
         holder += "<td>" + CarApp.cars[c]["year"] + "</td>";
-        holder += "<td>" + "<button onclick='CarApp.deleteCar(" + c + ");'>X</button><br/>" + "</td>";
+        holder += "<td>" + "<button onclick='CarApp.deleteCar(" + c + ");'class='btn btn-danger fa fa-trash-o'></button><br/>" + "</td>";
+        holder += "<td>"+"<div class='btn btn-warning' onclick='CarApp.Update.show("+c+")'>Edit</div>"
         holder += "</tr>";
 
     }
@@ -44,4 +45,31 @@ CarApp.deleteCar = function (index) {
     CarApp.cars.splice(index, 1);
     CarApp.drawTable();
 };
+//Update functionality
+CarApp.Update = {
+    //Calls modal to show user the info
+    show: function (
+        /*the index of the car you're about to edit*/
+        index) {
+        //Bring up Modal
+        var carEntry = CarApp.cars[index].model;
+        var carYear = CarApp.cars[index].year;
+        document.getElementById("editCarEntry").value = carEntry;
+        document.getElementById("editCarYear").value = carYear;
+        document.getElementById("editCarId").value = index;
+        $("#updateModal").modal();
+    },
+    save: function () {
+        $("#updateModal").modal("hide");
+        var carEntry = document.getElementById("editCarEntry").value;
+        var carYear = document.getElementById("editCarYear").value;
+        var carIndex = document.getElementById("editCarId").value;
+        CarApp.cars[carIndex].year = carYear;
+        CarApp.cars[carIndex].model = carEntry;
+        CarApp.drawTable();
+    }
+};
+
+
+    
 
